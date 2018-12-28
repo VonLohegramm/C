deff57eb611dbb9044eedb73047b9ea92b4a1325
VonLohegramm <35115169+VonLohegramm@users.noreply.github.com>
2018-12-28 16:44:16 -0200

    Add files via upload

--- Etc/Palindromo.c
+++ Etc/Palindromo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 bool checkPalindrome(char * inputString)
 {
@@ -20,13 +21,41 @@ bool checkPalindrome(char * inputString)
 	}
 }
 
+char *RemoveSpace(char palavra[])
+{
+	char *fixPalavra;
+	fixPalavra = (char*)calloc(0, sizeof(char));
+	int qtd = strlen(palavra);
+	int i, y = 0;
+	
+	for(i = 0; i < qtd; i++)
+	{
+		if(palavra[i] != ' ')
+		{	
+			fixPalavra[y] = palavra[i];	
+			y++;
+		}
+	}
+	return fixPalavra;
+	free(fixPalavra);
+}
+
 main()
 {
-	char pal[30];
+	char palOriginal[30], palCorreta[30];
+	int i;
 	
-	scanf("%s", pal);
+	fflush(stdin);
+	gets(palOriginal);
+	
+	strcpy(palCorreta, RemoveSpace(palOriginal));
+	
+	int qtd = strlen(palCorreta);
+	
+	for(i = 0; i < qtd; i++)
+		printf("| %c |", palCorreta[i]);
 
-	bool res = checkPalindrome(pal);
+	bool res = checkPalindrome(palCorreta);
 	
-	res ? printf("E ternario") : printf("nao eh ternario");
+	res ? printf("\nE polindromo") : printf("\nnao eh polindromo");
 }
